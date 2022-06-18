72c5dd7419839ac8d8b48b322fc46d7c3b119b98
surajverma2587 <surajverma2587@gmail.com>
2022-06-18 13:32:14 +0100

    add error handling

--- src/index.js
+++ src/index.js
@@ -6,35 +6,39 @@ const initDatabase = require("./db");
 const question = require("./questions");
 
 const init = async () => {
-  const { executeQuery, closeConnection } = await initDatabase({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+  try {
+    const { executeQuery, closeConnection } = await initDatabase({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
-  let inProgress = true;
+    let inProgress = true;
 
-  while (inProgress) {
-    const { dbAction } = await inquirer.prompt(question);
+    while (inProgress) {
+      const { dbAction } = await inquirer.prompt(question);
 
-    if (dbAction === "getUsers") {
-      const users = await executeQuery("SELECT * FROM users");
+      if (dbAction === "getUsers") {
+        const users = await executeQuery("SELECT * FROM users");
 
-      console.table(users);
+        console.table(users);
+      }
+
+      if (dbAction === "getBooks") {
+        const books = await executeQuery("SELECT * FROM ??", ["books"]);
+
+        console.table(books);
+      }
+
+      if (dbAction === "quit") {
+        await closeConnection();
+        inProgress = false;
+        console.log("THANK YOU");
+      }
     }
-
-    if (dbAction === "getBooks") {
-      const books = await executeQuery("SELECT * FROM ??", ["books"]);
-
-      console.table(books);
-    }
-
-    if (dbAction === "quit") {
-      await closeConnection();
-      inProgress = false;
-      console.log("THANK YOU");
-    }
+  } catch (error) {
+    console.log(`[ERROR]: Internal error | ${error.message}`);
   }
 };
 
